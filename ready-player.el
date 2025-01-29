3bb454a834e7d0f91bf27c957fc52ce15a17b739
xenodium <me+gh@xenodium.com>
2025-01-29 11:20:43 +0000

    Bumping version

--- ready-player.el
+++ ready-player.el
@@ -5,8 +5,8 @@
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; Package-Requires: ((emacs "28.1"))
 ;; URL: https://github.com/xenodium/ready-player
-;; Version: 0.29.1
-(defconst ready-player--version "0.29.1")
+;; Version: 0.30.1
+(defconst ready-player--version "0.30.1")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
